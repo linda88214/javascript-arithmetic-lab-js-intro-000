5b5209b2b6be3bd66f7c2aa68f1b89620fc66e78
Yerim Hu <yerimhu88@gmail.com>
2017-10-31 12:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ function add(a, b) {
   return a + b;
 }
 
-function add(a, b) {
+function subtract(a, b) {
   return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n++
 }
